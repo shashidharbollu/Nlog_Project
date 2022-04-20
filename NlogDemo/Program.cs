e63c82ad493dc8ea99c53d928cfeb5632f2905bc
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-21 00:36:08 +0530

    [shashidhar] ADD:NLOG file updated

---
+++ NlogDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+using System;
+using NlogDemo;
+namespace NlogDemo
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to nlog!");
+            AddNumber add = new AddNumber();
+            add.Sum(1, 2);
+    
+    }
+    }
+}
